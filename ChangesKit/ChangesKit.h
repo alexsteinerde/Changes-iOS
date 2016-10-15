9a550bdcff6fb57f616d04cc5d35925bfce33062
Alex Steiner <privat@alexsteiner.de>
2016-10-15 10:10:53 +0200

    Version 1.0 (1) - Cleanup - Added framework target - Added structure - Installed Alamofire pod

---
+++ ChangesKit/ChangesKit.h
@@ -0,0 +1,19 @@
+//
+//  ChangesKit.h
+//  ChangesKit
+//
+//  Created by Alex on 15/10/2016.
+//  Copyright © 2016 Alex Steiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChangesKit.
+FOUNDATION_EXPORT double ChangesKitVersionNumber;
+
+//! Project version string for ChangesKit.
+FOUNDATION_EXPORT const unsigned char ChangesKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChangesKit/PublicHeader.h>
+
+
